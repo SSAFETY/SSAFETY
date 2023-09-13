955166c57fe1e4a341719463f1c1499efbf3add3
YOOJUN <leehk77789@gmail.com>
2023-09-13 16:56:15 +0900

    (fe)Style: change css

--- ssafety/src/components/DetailMap.js
+++ ssafety/src/components/DetailMap.js
@@ -1,37 +1,45 @@
 import React, { useEffect, useRef } from 'react';
 import * as d3 from 'd3';
 import { feature } from 'topojson-client';
-import sung from '../mapData/sung.json'; // sung.json은 GeoJSON 데이터 파일입니다.
+import sung from '../mapData/skorea-municipalities-2018-topo.json';
+import '../css/Home.css';
 
-const DetailMap = () => {
+const SungMap = () => {
+  // svg를 그릴 엘리먼트 설정을 위한 ref
   const chart = useRef(null);
 
   const printD3 = () => {
-    const width = window.innerWidth; // 페이지 너비로 설정
-    const height = window.innerHeight; // 페이지 높이로 설정
+    // 부모 요소의 크기를 가져오도록 수정
+    const parentWidth = chart.current.clientWidth;
+    const parentHeight = chart.current.clientHeight;
 
+    // 메르카토르 투영법 설정
     const projection = d3.geoMercator().scale(1).translate([0, 0]);
     const path = d3.geoPath().projection(projection);
-    const featureData = feature(sung, sung.objects['gu']); // 데이터 소스에 맞게 수정
-    const bounds = path.bounds(featureData);
 
+    const featureData = feature(sung, sung.objects['skorea_municipalities_2018_geo']);
+
+    const filteredFeatures = featureData.features.filter((feature) =>
+      feature.properties.name.startsWith('서울시')
+    );
+
+    // 필터링된 데이터의 경계를 계산합니다.
+    const bounds = path.bounds({ type: 'FeatureCollection', features: filteredFeatures });
+
+    // svg의 크기에 따른 지도의 크기와 위치값을 설정합니다.
     const dx = bounds[1][0] - bounds[0][0];
     const dy = bounds[1][1] - bounds[0][1];
     const x = (bounds[0][0] + bounds[1][0]) / 2;
     const y = (bounds[0][1] + bounds[1][1]) / 2;
-    const scale = 0.9 / Math.max(dx / width, dy / height);
-    const translate = [width / 2 - scale * x, height / 2 - scale * y];
+    const scale = 0.9 / Math.max(dx / parentWidth, dy / parentHeight);
+    const translate = [parentWidth / 2 - scale * x, parentHeight / 2 - scale * y];
 
     projection.scale(scale).translate(translate);
 
-    const svg = d3.select(chart.current).append('svg').attr('width', width).attr('height', height);
+    // svg를 만들고
+    const svg = d3.select(chart.current).append('svg').attr('width', parentWidth).attr('height', parentHeight);
 
     const mapLayer = svg.append('g');
-
-    // SIG_KOR_NM이 성남시로 시작하는 지역만 필터링합니다.
-    const filteredFeatures = featureData.features.filter((feature) =>
-      feature.properties.SIG_KOR_NM.startsWith('성남시')
-    );
 
     // 필터링된 지역만을 지도에 표시합니다.
     mapLayer
@@ -40,15 +48,14 @@ const DetailMap = () => {
       .enter()
       .append('path')
       .attr('d', path)
-      .style('fill', 'skyblue') // 기본 색상 설정
-      .style('transition', 'transform 0.2s'); // 트랜지션 효과 추가
+      .style('fill', '#666');
   };
 
   useEffect(() => {
     printD3();
   }, []);
 
-  return <div ref={chart} className="detail-map"></div>;
+  return <div ref={chart} style={{ width: '100%', height: '100%' }}></div>;
 };
 
-export default DetailMap;
+export default SungMap;
