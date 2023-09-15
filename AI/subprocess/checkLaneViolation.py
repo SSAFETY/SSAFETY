5b0c8bd87dcf0b876a0ff6b50db40d9d965f3ea0
김용균 <kyg4199@naver.com>
2023-09-15 16:28:22 +0900

    (AI)Feat: subprocess 최종 파일 추가

--- AI/subprocess/checkLaneViolation.py
+++ AI/subprocess/checkLaneViolation.py
@@ -11,18 +11,27 @@ def call_conda_environment():
     try:
         # Conda 환경 활성화 명령어 설정
         #activate_cmd = f"conda activate {conda_env_name}"
+        ENV_NAME = "test"
 
         # Conda 환경을 활성화
         #subprocess.run('conda activate test && python /nia-82-134-main/service-example.py', shell=True)
-        subprocess.call('conda activate test', shell=True)
+        #subprocess.run("source /anaconda3/etc/profile.d/conda.sh", shell=True)
+        #subprocess.run(f"""conda init bash
+        #conda activate {ENV_NAME}
+        #python -c 'import pandas; print(pandas.__version__)'""", shell=True, executable='/bin/bash', check=True)
+        anaconda_path = "/home/ssafy01/anaconda3"
+        conda_script_path = f"{anaconda_path}/etc/profile.d/conda.sh"
+        # Anaconda를 실행하는 셸 스크립트 작성
+        conda_command = f"source {conda_script_path} && conda activate {ENV_NAME}"
+        subprocess.run(conda_command, shell=True, executable='/bin/bash', check=True)
+        python_executable = f"{anaconda_path}/envs/test/bin/python3"
+        python_script_path = "/home/ssafy01/nia-82-134-main/service-example.py"
+        subprocess.run(f"{python_executable} {python_script_path}", shell=True, executable='/bin/bash', check=True)
         rospy.loginfo(f"Conda 환경 {conda_env_name}이(가) 활성화되었습니다.")
 
         # 필요한 스크립트 실행
         #subprocess.check_call(["python", "/nia-82-134-main/service-example.py"])
-        #subprocess.run(['python', '/nia-82-134-main/service-example.py'], 
-        #stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
 
-        
         # Conda 환경 비활성화
         #deactivate_cmd = "conda deactivate"
         #subprocess.check_call(["bash", "-c", deactivate_cmd], shell=True)
