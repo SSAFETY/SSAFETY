c1a19724041564f39feb2ae1539b13ab91ce402a
김용균 <kyg4199@naver.com>
2023-09-20 17:27:48 +0900

    (AI)Feat: 차량 이미지 저장 및 번호판 저장

--- AI/model_parcing/service-example.py
+++ AI/model_parcing/service-example.py
@@ -317,7 +317,7 @@ def detect_violation(object_info, violation_model, device):
 
 
 # # Config 파일 지정 및 학습된 모델 경로 지정
-output_filename = './parsing/normal_cut.mp4'
+output_filename = './parsing/normal_cut.webm'
 
 def parsing():
     global new_fps, main_web_mp4, output_filename
@@ -328,12 +328,9 @@ def parsing():
     cut_duration_seconds = 10
     cut_frame_count = new_fps * cut_duration_seconds
 
-    output_fourcc = cv2.VideoWriter_fourcc(*'mp4v')  # 저장할 동영상 코덱 설정
+    output_fourcc = cv2.VideoWriter_fourcc(*'VP80')  # 저장할 동영상 코덱 설정
     
     parcing_out = cv2.VideoWriter(output_filename, output_fourcc, new_fps, (int(vidcap.get(3)), int(vidcap.get(4))))
-
-    print("TOTAL :",total_frames)
-    print("par : ", total_frames - cut_frame_count)
 
     for i in range(total_frames):
         ret, frame = vidcap.read()
@@ -345,9 +342,49 @@ def parsing():
     vidcap.release()
     parcing_out.release()
 
+def make_plate(resized_frame, poly):
+
+    # poly 배열을 사용하여 mask 생성
+    mask = np.zeros_like(resized_frame)
+    cv2.fillPoly(mask, [poly.astype(np.int32).reshape(-1, 2)], (255, 255, 255))
+
+    # mask를 사용하여 bbox 부분 자르기
+    cropped_image = cv2.bitwise_and(resized_frame, mask)
+
+    # 저장할 경로 및 파일 이름 설정
+    save_path = 'cropped_image.jpg'
+
+    # 이미지 저장
+    cv2.imwrite(save_path, cropped_image)
+    print(f'이미지 저장 완료: {save_path}')
+    # 이미지 파일 경로
+    image_path = 'cropped_image.jpg'
+
+    # 이미지 파일 로드
+    image = cv2.imread(image_path)
+
+    # 이미지를 흑백으로 변환
+    gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+    # 번호판 검출을 위해 Haar Cascade 분류기 사용
+    plate_cascade = cv2.CascadeClassifier(cv2.data.haarcascades +'haarcascade_russian_plate_number.xml')
+
+    # 번호판 검출
+    plates = plate_cascade.detectMultiScale(gray_image)
+
+    for i, (x, y, w, h) in enumerate(plates):
+        # 번호판 영역 자르기
+        plate_roi = image[y:y+h, x:x+w]
+        
+        # 번호판 이미지 저장
+        plate_image_path = os.path.join('./', f'plate_{i}.jpg')
+        cv2.imwrite(plate_image_path, plate_roi)
+
+
 # In[4]:
 
-path = '/home/ssafy/nia-82-134-main'
+path = '/home/ssafy01/nia-82-134-main'
+main_web_mp4 = './parsing/webcam_out.mp4'
 
 vehicle_cfg_path = f'{path}/configs/vehicle_detection_config.py'
 vehicle_ckpt_path = f"{path}/best_models/vehicle_detection_model.pth"
@@ -400,12 +437,12 @@ curr_img = Image.open(img_path)
 result_img = None
 
 # 새로운 FPS 값을 설정
-new_fps = 10.0
+new_fps = 10
 
 # 웹캠 열기
-main_web_mp4 = './parsing/webcam_out_DIVX.avi'
-webcam = cv2.VideoCapture(0)
-fourcc = cv2.VideoWriter_fourcc(*'DIVX')
+# webcam = cv2.VideoCapture(0)
+webcam = cv2.VideoCapture("/home/ssafy01/Downloads/20230920_153715 (1).mp4")
+fourcc = cv2.VideoWriter_fourcc(*'mp4v')
 webcam_out = cv2.VideoWriter(main_web_mp4, fourcc, new_fps, (1280, 720))
 
 if not webcam.isOpened():
@@ -422,7 +459,8 @@ violation_frame = 0
 violation_flag = False
 
 wait_frame = 0
-wait_flag = True
+#True로 변경해야 함
+wait_flag = False
 
 
 time_interval = 5
@@ -430,10 +468,7 @@ time_interval = 5
 print(os.getcwd())
 
 if not os.path.isdir('./parsing'):
-    os.mkdir('./parsing')
-
-if os.path.exists(output_filename):
-    os.remove(output_filename)
+        os.mkdir('./parsing')
 
 while webcam.isOpened():
     status, frame = webcam.read()
@@ -451,11 +486,9 @@ while webcam.isOpened():
             webcam_out.release()
 
             parsing()
-
-            data = {"fileName": "/home/ssafy01/Downloads/2023-09-18-172106.mp4"}
+            data = {"fileName": "/home/ssafy01/nia-82-134-main/parsing/normal_cut.webm"}
             headers = {'Content-Type': 'application/json'}
-            #response = requests.post("http://localhost:8080/api/hi", data=json.dumps(data), headers=headers)
-
+            # response = requests.post("http://localhost:8080/api/hi", data=json.dumps(data), headers=headers)
             wait_flag = True
             wait_frame = 0
 
@@ -482,15 +515,11 @@ while webcam.isOpened():
                     labels = [obj.label for obj in result_list]
                     print(labels)
                     if "violation" in labels:
+                        vehicle_polygons = [obj.poly for obj in image_info.objects if obj.obj_type == 'vehicle']
+                        make_plate(resized_frame, vehicle_polygons[0])
 
                         violation_frame = 0
                         violation_flag = True
-            else :
-                print("exist") 
-
-
-            
-                
 
         #image_info.objects = result_list
         #result_img = viz_inference_result(resized_frame, image_info)        
@@ -504,5 +533,4 @@ while webcam.isOpened():
 
 webcam.release()
 webcam_out.release()
-print(webcam_out)
 cv2.destroyAllWindows()
