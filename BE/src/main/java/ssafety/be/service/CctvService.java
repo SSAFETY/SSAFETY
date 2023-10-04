5b0668569cf45f69f9a97eec18668dd74f4b72a8
YOOJUN <leehk77789@gmail.com>
2023-10-04 14:54:05 +0900

    (BE) Chore: fix live

--- BE/src/main/java/ssafety/be/service/CctvService.java
+++ BE/src/main/java/ssafety/be/service/CctvService.java
@@ -7,7 +7,7 @@ import org.springframework.web.client.RestTemplate;
 @Service
 public class CctvService {
 
-    @Value("${cctv.apiKey}")
+    @Value("0db19b82b4d84ef3846c147df0b0028d")
     private String apiKey;
 
     public String fetchCCTVData() {
@@ -17,10 +17,10 @@ public class CctvService {
                     "?apiKey=" + apiKey +
                     "&type=ex" +
                     "&cctvType=1" +
-                    "&minX=127.100000" +
-                    "&maxX=128.890000" +
-                    "&minY=34.100000" +
-                    "&maxY=39.100000" +
+                    "&minX=126.734086" +
+                    "&maxX=127.269311" +
+                    "&minY=37.413294" +
+                    "&maxY=37.715133" +
                     "&getType=json";
 
             RestTemplate restTemplate = new RestTemplate();
@@ -31,4 +31,3 @@ public class CctvService {
         }
     }
 }
-
