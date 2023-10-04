8b665911ebc140037a45c7ecae2a245b24fb0d5b
YOOJUN <leehk77789@gmail.com>
2023-10-04 17:05:15 +0900

    (BE) Chore: fix live

--- FE/ssafety/src/components/Cctv.js
+++ FE/ssafety/src/components/Cctv.js
@@ -10,7 +10,6 @@ const Cctv = () => {
     axios
       .get('https://j9a102.p.ssafy.io/api/cctv/data')
       .then((response) => {
-        // response.data.data를 사용하여 cctvData 배열 설정
         setCCTVData(response.data);
         console.log(response.data);
         setLoading(false);
@@ -22,24 +21,23 @@ const Cctv = () => {
   }, []);
 
   // 첫 번째 CCTV 데이터 가져오기
-  const firstCctv = cctvData.length > 0 ? cctvData[0].cctvurl : null;
+  const firstCctv = cctvData[0];
   console.log(firstCctv)
+  console.log(firstCctv.cctvurl)
 
   return (
     <div>
       <h1>CCTV Data</h1>
-      {/* {loading ? (
+      {loading ? (
         <p>Loading...</p>
-      ) : firstCctv ? (
+      ) : (
         <div>
           <video controls width="640" height="360">
             <source src={firstCctv.cctvurl} type="application/x-mpegURL" />
             Your browser does not support the video tag.
           </video>
         </div>
-      ) : (
-        <p>No CCTV data available.</p>
-      )} */}
+      )}
     </div>
   );
 };
