8bd07fbdc50abdf46914a1bc48265d3ee9d97638
YOOJUN <leehk77789@gmail.com>
2023-10-05 22:01:05 +0900

    (FE) Chore: fix map

--- FE/ssafety/src/components/BusMap.js
+++ FE/ssafety/src/components/BusMap.js
@@ -81,7 +81,7 @@ const BusMap = () => {
     const x = (bounds[0][0] + bounds[1][0]) / 2;
     const y = (bounds[0][1] + bounds[1][1]) / 2;
     const scale = 1.5 / Math.max(dx / width, dy / height);
-    const translate = [width / 2 - scale * x + 40, height / 2 - scale * y + 300];
+    const translate = [width / 2 - scale * x + 60, height / 2 - scale * y + 320];
     projection.scale(scale).translate(translate);
 
     const svg = d3.select(chart.current).selectAll('svg').remove();
