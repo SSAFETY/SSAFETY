bba3f06d714938c5bd6c3a002a026801a1d6f091
YOOJUN <leehk77789@gmail.com>
2023-10-05 21:50:56 +0900

    (FE) Feat: Add logo link

--- FE/ssafety/src/components/NavBar.js
+++ FE/ssafety/src/components/NavBar.js
@@ -8,7 +8,9 @@ const NavBar = () => {
     <AppBar className="navbar">
       <Toolbar>
         <Typography variant="h6" component="div" className="logo">
-          <img src="https://a102.s3.ap-northeast-2.amazonaws.com/%EB%A1%9C%EA%B3%A0%2B%EB%84%A4%EC%9E%84.png" alt="로고" className="logo-image" />
+        <Link to="/">
+            <img src="https://a102.s3.ap-northeast-2.amazonaws.com/%EB%A1%9C%EA%B3%A0%2B%EB%84%A4%EC%9E%84.png" alt="로고" className="logo-image" />
+          </Link>
         </Typography>
         <div className="nav-links">
           <Button color="inherit" component={Link} to="/">
